7cae61acf51ea1cb89930744ae3a834c86a2b45b
Allen Pan <allpan@email.com>
2023-04-21 18:30:36 -0700

    Update tstech28 flow

--- vlsi/tstech28.mk
+++ vlsi/tstech28.mk
@@ -1,15 +1,22 @@
 design    ?= default
 ENV_YML   ?= $(vlsi_dir)/env-bwrc.yml
 TECH_CONF ?= tstech28.yml
+# Include the srams used in Verilog source code
+HAMMER_EXTRA_ARGS += -p $(vlsi_dir)/tstech28_sram_macros/sram_generator-output.yml
+
+GENERATED_CONFS += $(GENERATED_CONFS)
+
+USE_SRAM_COMPILER ?= 1
 
 ifeq ($(design),default)
     tech_name         ?= tstech28
     CONFIG            ?= HPURocketConfig
-    DESIGN_CONFS      ?= 
+    # CONFIG            ?= RocketConfig
+    # DESIGN_CONFS      ?= 
     TOP               ?= ChipTop
     # CUSTOM_VLOG       =
     # TOP_MACROCOMPILER_MODE ?= -l $(SMEMS_CACHE) -hir $(SMEMS_HAMMER) 
 
 endif
 
-INPUT_CONFS       ?= $(TOOLS_CONF) $(TECH_CONF) $(DESIGN_CONFS) $(EXTRA_CONFS)
+INPUT_CONFS       ?= $(TOOLS_CONF) $(TECH_CONF) $(DESIGN_CONFS)
