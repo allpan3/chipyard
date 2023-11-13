6b2ccaf1eb4332dbf506730bf439431a4a17b8a8
Allen Pan <allpan@email.com>
2023-11-13 07:40:41 -0800

    Add build specs

--- vlsi/tstech28.mk
+++ vlsi/tstech28.mk
@@ -1,3 +1,4 @@
+HAMMER_EXEC := ./hammer-nvsa.py
 design    ?= default
 ENV_YML   ?= $(vlsi_dir)/env-bwrc.yml
 TECH_CONF ?= tstech28.yml
@@ -9,11 +10,24 @@ USE_SRAM_COMPILER ?= 1
 
 ifeq ($(design),default)
     tech_name         ?= tstech28
-    CONFIG            ?= HPURocketConfig
+    CONFIG            ?= CustomHPUSoCConfig
     # DESIGN_CONFS      ?= 
     TOP               ?= ChipTop
     # CUSTOM_VLOG       =
     # TOP_MACROCOMPILER_MODE ?= -l $(SMEMS_CACHE) -hir $(SMEMS_HAMMER) 
+endif
+
+ifeq ($(design),gemmini)
+    tech_name         ?= tstech28
+    CONFIG            ?= CustomGemminiSoCConfig
+    TOP               ?= ChipTop
+endif
+
+
+ifeq ($(design),boom)
+    tech_name         ?= tstech28
+    CONFIG            ?= LargeBoomConfig
+    TOP               ?= ChipTop
 endif
 
 INPUT_CONFS       ?= $(TOOLS_CONF) $(TECH_CONF) $(DESIGN_CONFS)
